6b3f3a81947ba9d652952693a160c7a592f2d84f
ishiduca <ishiduca@gmail.com>
2016-02-04 20:49:03 +0900

    add test to t/02_add.js

--- t/02_add.js
+++ t/02_add.js
@@ -1,28 +1,154 @@
 'use strict'
 var test    = require('tape')
+var stream  = require('readable-stream')
 var through = require('through2')
 var Clazy   = require('../index')
-
 var fs      = require('fs')
 var path    = require('path')
 var mkdirp  = require('mkdirp')
 var remove  = require('remove')
 
-var savedDir = path.join(__dirname, 'copies')
+test('lazyStream.add(readableStream)', (t) => {
+    var ping = []
+    var lazyStream = Clazy()
 
-function setup (f) {
-    mkdirp(savedDir, f)
-}
+    lazyStream
+      .on('data', (data) => {
+        ping.push(data)
+      })
+      .on('end', () => {
+        t.ok(1, 'lazyStream ended')
+        t.deepEqual(ping, ["0","1","2","3","4","5","6","7","8"]
+          , 'ping deepEq  ["0","1","2","3","4","5","6","7","8"]')
+        t.end()
+      })
 
-function teardown (end) {
-    remove(savedDir, (err) => {
-        err ? console.error(err)
-            : console.log('# teardown remove "%s"', savedDir)
-        end()
+    ;('123456789').split('').forEach((n) => {
+        var rs = new stream.Readable
+        rs._read = function () {}
+
+        lazyStream.add(rs.on('data', (n) => {/*dummp*/}))
+
+        rs.push(n)
+        rs.push(null)
     })
-}
+})
+
+test('lazyStream.add(readableStream) # with error', (t) => {
+    var ping = []
+    var errs = []
+    var lazyStream = Clazy()
+
+    lazyStream
+      .on('data', (data) => {
+        ping.push(data)
+      })
+      .on('error', (err) => {
+        errs.push(err.message)
+      })
+      .on('end', () => {
+        t.ok(1, 'lazyStream ended')
+        t.deepEqual(errs, ["count: 5, ping: 4"], 'errs deepEq ["count: 5, ping: 4"]')
+        t.deepEqual(ping, ["0","1","2","3","5","6","7","8"]
+          , 'ping deepEq  ["0","1","2","3","5","6","7","8"]')
+        t.end()
+      })
+
+    ;('123456789').split('').forEach((n) => {
+        var rs = new stream.Readable
+        rs._read = function () {}
+
+        lazyStream.add(rs.on('data', (n) => {/*dummp*/}))
+
+        if (n === '5') {
+            return rs.emit('error', new Error('count: 5, ping: 4'))
+        }
+
+        rs.push(n)
+        rs.push(null)
+    })
+})
+
+test('lazyStream.add(writableStream)', (t) => {
+    var ping = []
+    var lazyStream = Clazy()
+
+    lazyStream
+      .on('data', (data) => {
+        ping.push(data)
+      })
+      .on('end', () => {
+        t.ok(1, 'lazyStream ended')
+        t.deepEqual(ping, ["0","1","2","3","4","5","6","7","8"]
+          , 'ping deepEq  ["0","1","2","3","4","5","6","7","8"]')
+        t.end()
+      })
+
+    ;('123456789').split('').forEach((n) => {
+        var ws = new stream.Writable
+        ws._write = function (data, enc, done) {
+            setTimeout(done, 10)
+        }
+
+        lazyStream.add(ws)
+
+        ws.end(n)
+    })
+})
+
+test('lazyStream.add(writableStream) # with error', (t) => {
+    var ping = []
+    var errs = []
+    var lazyStream = Clazy()
+
+    lazyStream
+      .on('data', (data) => {
+        ping.push(data)
+      })
+      .on('error', (err) => {
+        errs.push(err.message)
+      })
+      .on('end', () => {
+        t.ok(1, 'lazyStream ended')
+        t.deepEqual(ping, ["0","1","3","4","6","7"]
+          , 'ping deepEq  ["0","1","3","4","6","7"]')
+        t.deepEqual(errs, ["count: 3, ping: 2", "count: 6, ping: 5", "count: 9, ping: 8"]
+          , 'errs deepEq ["count: 3, ping: 2", "count: 6, ping: 5", "count: 9, ping: 8"]')
+        t.end()
+      })
+
+    ;('123456789').split('').forEach((n, i) => {
+        var ws = new stream.Writable
+        ws._write = function (data, enc, done) {
+            setTimeout(() => {
+                var err
+                if (0 === data % 3)
+                    err = new Error('count: ' + n + ', ping: ' + i)
+                done(err)
+            }, 10)
+        }
+
+        lazyStream.add(ws)
+
+        ws.end(n)
+    })
+})
 
 test('lazyStream.add(stream)', (t) => {
+    var savedDir = path.join(__dirname, 'copies')
+
+    function setup (f) {
+        mkdirp(savedDir, f)
+    }
+
+    function teardown (end) {
+        remove(savedDir, (err) => {
+            err ? console.error(err)
+                : console.log('# teardown remove "%s"', savedDir)
+            end()
+        })
+    }
+
     setup((err) => {
         if (err) {
             console.error(err)
@@ -59,41 +185,5 @@ test('lazyStream.add(stream)', (t) => {
 
             lazyStream.add(readStream.pipe(writeStream))
         })
-    })
-})
-
-var stream = require('readable-stream')
-
-test('lazyStream.add(stream) # with error', (t) => {
-    var ping  = []
-    var errs = []
-    var lazyStream = Clazy()
-
-    lazyStream
-        .on('error', (err) => {
-            errs.push(err.message)
-        })
-        .on('end', () => {
-            t.ok(1, 'lazyStream.ended')
-            t.deepEqual(errs, ['number 2', 'number 4'], 'errs deepEq ["number 2", "number 4"]')
-            t.deepEqual(ping, ['0', '2', '4'], 'ping deepEq ["0", "2", "4"]')
-            t.end()
-        })
-        .pipe(through.obj((n, _, done) => {
-            ping.push(n)
-            done()
-        }))
-
-    ;[1, 2, 3, 4, 5].forEach((n) => {
-        var ws = new stream.Writable({objectMode: true})
-        ws._write = function (n, enc, done) {
-            if (n % 2 === 0)
-                return done(new Error('number ' + n))
-            done()
-        }
-
-        lazyStream.add(ws)
-
-        ws.end(n)
     })
 })
